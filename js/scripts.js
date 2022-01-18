ac2b0490e81fe11c413736c8af7c3564ee32c726
Kevin Williams <kevin@k9w.org>
2022-01-17 20:25:55 -0800

    Initial commit.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// formula: (0°C × 9/5) + 32 = 32°F
+function celsiusToFahrenheit(celsiusInput) {
+  return (celsiusInput * 9/5) + 32 = 32°F;
+}
+
+// formula: (32°F − 32) × 5/9 = 0°C
+function fahrenheitToCelsius(fahrenheitInput) {
+  return (fahrenheitInput − 32) × 5/9 = 0°C;
+}
+
+const celsiusInput = parseInt(prompt("Enter a temperature in Celsius:"));
+const fahrenheitInput = parseInt(prompt("Enter a temperature in Fahrenheit:"));
+
+alert(celsiusToFahrenheit(celsiusInput));
+alert(fahrenheitToCelsius(fahrenheitInput));
